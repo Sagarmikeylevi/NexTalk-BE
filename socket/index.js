360b26a3a196acc7e7d56757ca26918068dc67d0
unknown <sagarmikeylevi@gmail.com>
2024-02-18 15:38:43 +0530

    completed send and recived real time chat using socket io

---
+++ socket/index.js
@@ -0,0 +1,38 @@
+const { Server } = require("socket.io");
+
+const io = new Server({
+  cors: "http://localhost:5173",
+});
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find(
+      (user) => user.userId === message.secondPersonId
+    );
+
+    if (user) {
+      io.to(user.socketId).emit("getMessage", message);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+});
+
+io.listen(3000);
